2adeb3ec73e8e283b4cba4579f69be895b9f742d
Денис Попович <popov.den30@gmail.com>
2021-04-08 11:57:31 +0300

    Added global log method

--- GTA_SA_PathsRedactor/App.xaml.cs
+++ GTA_SA_PathsRedactor/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Windows;
+using Serilog;
 
 namespace GTA_SA_PathsRedactor
 {
@@ -15,14 +16,26 @@ namespace GTA_SA_PathsRedactor
         {
             bool createdNew;
 
-            mutex = new Mutex(true, "Path redactor", out createdNew);
+            mutex = new Mutex(true, "Path editor", out createdNew);
 
             if (!createdNew)
             {
                 this.Shutdown();
             }
 
+            ConfigureLogger();
+
             DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void ConfigureLogger()
+        {
+            var logConfiguration = new LoggerConfiguration();
+
+            Log.Logger = logConfiguration.WriteTo.File(System.IO.Path.Combine(Environment.CurrentDirectory, "log.txt"),
+                                                       outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}{NewLine}",
+                                                       restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information)
+                                         .CreateLogger();
         }
 
         private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
@@ -30,6 +43,31 @@ namespace GTA_SA_PathsRedactor
             MessageBox.Show(e.Exception.Message);
 
             e.Handled = true;
+        }
+
+        public static void LogErrorInfoAndShowMessageBox(string userMessage, string logMessage, Exception? ex = null)
+        {
+            MessageBox.Show(userMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            if (ex == null)
+                Log.Error(logMessage);
+            else
+                Log.Error(ex, logMessage);
+        }
+        public static void LogErrorInfoAndShowMessageBox(string userMessage, Exception? ex = null)
+        {
+            MessageBox.Show(userMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            if (ex == null)
+                Log.Error(userMessage);
+            else
+                Log.Error(ex, userMessage);
+        }
+        public static void LogErrorInfoAndShowMessageBox(Exception ex)
+        {
+            MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            Log.Error(ex, "{Message}");
         }
     }
 }
