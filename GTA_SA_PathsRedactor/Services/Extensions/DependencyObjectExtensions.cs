bae65943f642b11e82af1a1b78d5bf8029b14c40
Денис Попович <popov.den30@gmail.com>
2021-04-25 11:24:26 +0300

    Moved find visual child method and fixed UI

---
+++ GTA_SA_PathsRedactor/Services/Extensions/DependencyObjectExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace GTA_SA_PathsRedactor.Services
+{
+    public static class DependencyObjectExtensions
+    {
+        public static childItem? FindVisualChild<childItem>(this DependencyObject? obj)
+                      where childItem : DependencyObject
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is childItem)
+                {
+                    return (childItem)child;
+                }
+                else
+                {
+                    childItem? childOfChild = FindVisualChild<childItem>(child);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+            return null;
+        }
+    }
+}
