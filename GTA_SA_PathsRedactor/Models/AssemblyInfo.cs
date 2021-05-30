e63e93d4d99edcb2fb81a314ee7fbd801566cd5c
Денис Попович <popov.den30@gmail.com>
2021-05-30 13:10:42 +0300

    Added new models for setting window

---
+++ GTA_SA_PathsRedactor/Models/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿namespace GTA_SA_PathsRedactor.Models
+{
+    public record AssemblyInfo(string Title, string FullName, string Version,
+                               string Author, string Company, string Product, string Description);
+}
---
+++ GTA_SA_PathsRedactor/Models/ItemInfo.cs
@@ -0,0 +1,4 @@
+﻿namespace GTA_SA_PathsRedactor.Models
+{
+    public record ItemInfo(string Name, string Purpose, string Description);
+}
